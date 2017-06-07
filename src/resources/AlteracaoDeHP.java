fed07fbda3972062c02925c1bcb4e58652046f96
Filipe Barros Rodrigues <filiperodrigues97@hotmail.com>
2017-06-07 13:15:53 -0300

    Transformado em projeto do Net Beans

--- src/resources/AlteracaoDeHP.java
+++ src/resources/AlteracaoDeHP.java
@@ -1,7 +1,5 @@
 // ## Implementation preserve start class opening. 
 // ## Implementation preserve end class opening. 
-import Ator;
-import Efeito;
 // ## Implementation preserve start class import. 
 // ## Implementation preserve end class import. 
 
@@ -19,12 +17,13 @@ public class AlteracaoDeHP extends Efeito
     /**
      * Operation
      *
-     * @param 
-     * @param 
+     * @param a
+     * @param a
      * @return 
      */
-    public AlteracaoDeHP ( String , int  )
+    public AlteracaoDeHP (String descricao, int quantidade)
     {
+        super(descricao);
         // ## Implementation preserve start class method.AlteracaoDeHP@@@@String@int 
         // ## Implementation preserve end class method.AlteracaoDeHP@@@@String@int 
     }
@@ -43,10 +42,11 @@ public class AlteracaoDeHP extends Efeito
      * @param 
      * @return boolean
      */
-    public boolean aplicar ( Ator  )
+    public boolean aplicar ( Ator alvo )
     {
         // ## Implementation preserve start class method.aplicar@boolean@@@Ator 
         // ## Implementation preserve end class method.aplicar@boolean@@@Ator 
+        return false;
     }
     /**
      * Operation
@@ -57,6 +57,7 @@ public class AlteracaoDeHP extends Efeito
     {
         // ## Implementation preserve start class method.informarEfeitoAplicado@String@@ 
         // ## Implementation preserve end class method.informarEfeitoAplicado@String@@ 
+        return "";
     }
     // ## Implementation preserve start class other.operations. 
     // ## Implementation preserve end class other.operations. 
